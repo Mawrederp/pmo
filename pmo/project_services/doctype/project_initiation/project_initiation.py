d4f48e18c2b6993af6f87b361b0f6c809d143760
Ahmed <final.option@hotmail.com>
2018-04-08 15:48:14 +0300

    project initiation

--- pmo/project_services/doctype/project_initiation/project_initiation.py
+++ pmo/project_services/doctype/project_initiation/project_initiation.py
@@ -7,4 +7,23 @@ import frappe
 from frappe.model.document import Document
 
 class ProjectInitiation(Document):
-	pass
+	
+	def on_submit(self):
+		frappe.get_doc({
+			"doctype": "Project",
+			"name": self.project_name,
+			"project_name": self.project_name,
+			"status": "Initiation"
+		}).save(ignore_permissions = True)
+
+		frappe.db.commit()
+
+		pp = frappe.get_doc({
+			"doctype": "Project Planning",
+			"project": self.project_name
+		}).save(ignore_permissions = True)
+
+		frappe.db.commit()
+
+		frappe.msgprint(_("Project {project} and Project Planning {pp} have been created".format(project=self.project_name, pp = pp)))
+
