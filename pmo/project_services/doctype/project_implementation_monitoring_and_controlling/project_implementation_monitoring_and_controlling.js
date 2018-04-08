ee2008b15deacb44de959dde4d3a4e029c66bc9c
Ahmed Madi <eng.ahmed-madi@hotmail.com>
2018-04-08 09:08:17 +0000

    initiat Project services module

---
+++ pmo/project_services/doctype/project_implementation_monitoring_and_controlling/project_implementation_monitoring_and_controlling.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, s and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Project Implementation Monitoring and Controlling', {
+	refresh: function(frm) {
+
+	}
+});
