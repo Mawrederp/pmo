13032b4c6231ca60d3183e39bd8cf9fd1b6733a2
Ahmed <final.option@hotmail.com>
2018-04-09 10:20:12 +0300

    project initiation, adding project state custom field

--- pmo/project_services/doctype/project_initiation/project_initiation.py
+++ pmo/project_services/doctype/project_initiation/project_initiation.py
@@ -4,26 +4,25 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class ProjectInitiation(Document):
-	pass
-	# def on_submit(self):
-	# 	frappe.get_doc({
-	# 		"doctype": "Project",
-	# 		"name": self.project_name,
-	# 		"project_name": self.project_name,
-	# 		"status": "Initiation"
-	# 	}).save(ignore_permissions = True)
+	
+	def on_submit(self):
+		frappe.get_doc({
+			"doctype": "Project",
+			"project_name": self.project_name
+		}).save(ignore_permissions = True)
 
-	# 	frappe.db.commit()
+		frappe.db.commit()
 
-	# 	pp = frappe.get_doc({
-	# 		"doctype": "Project Planning",
-	# 		"project": self.project_name
-	# 	}).save(ignore_permissions = True)
+		pp = frappe.get_doc({
+			"doctype": "Project Planning",
+			"project": self.project_name
+		}).save(ignore_permissions = True)
 
-	# 	frappe.db.commit()
+		frappe.db.commit()
 
-	# 	frappe.msgprint(_("Project {project} and Project Planning {pp} have been created".format(project=self.project_name, pp = pp)))
+		frappe.msgprint(_("Project {project} and Project Planning {pp} have been created".format(project=self.project_name, pp = pp)))
 
