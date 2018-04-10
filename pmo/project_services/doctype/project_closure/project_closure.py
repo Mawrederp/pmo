971be9c5cfa4f0dddc0a41ff5beb6d641300ce8c
Ammar Harara <ah1995@students.iugaza.edu.ps>
2018-04-10 11:35:02 +0300

    Project Closure Functionality, edit PIMC doctype

--- pmo/project_services/doctype/project_closure/project_closure.py
+++ pmo/project_services/doctype/project_closure/project_closure.py
@@ -7,4 +7,14 @@ import frappe
 from frappe.model.document import Document
 
 class ProjectClosure(Document):
-	pass
+	def on_submit(self):
+		doc = frappe.get_doc("Project",self.project_name)
+        doc.state = "Closed"
+		doc.status = "Completed"
+		doc.save(ignore_permissions = True)
+
+		frappe.db.commit()
+
+		msg = """Project Closure has been created: <b><a href="#Form/Project Closure/{0}">{0}</a></b>""".format(doc.name)
+        frappe.msgprint(msg)
+
