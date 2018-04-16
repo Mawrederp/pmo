dfb883582fef70ea5d26d6f351b21bedc8272ed8
Omar Jaber <omar.ja93@gmail.com>
2018-04-16 15:48:51 +0300

    Projects editing

--- pmo/project_services/doctype/project_planning/project_planning.js
+++ pmo/project_services/doctype/project_planning/project_planning.js
@@ -20,7 +20,7 @@ frappe.ui.form.on('Project Planning', {
         });
 
 
-        frm.add_custom_button(__("Project Implementation Monitoring and Controlling"), function () {
+        frm.add_custom_button(__("Project Implementation, Monitoring and Controlling"), function () {
             frappe.call({
                 "method": "existing_project_controlling",
                 doc: cur_frm.doc,
@@ -44,7 +44,11 @@ frappe.ui.form.on('Project Planning', {
         });
 
         $(".layout-main-section .form-inner-toolbar button:nth-child(2)").removeClass("btn-default");
-
+        $('.layout-main-section .form-inner-toolbar :nth-child(1)').before('<b><p style="text-align: center;font-size: 25px;">Project Phases</p></b>');
+        $('.layout-main-section-wrapper .layout-main-section .form-inner-toolbar').after('<style>.layout-main-section-wrapper .layout-main-section .form-inner-toolbar{height: 100px !important;}</style>');
+        $('.layout-main-section-wrapper .layout-main-section .form-inner-toolbar').after('<style>.layout-main-section-wrapper .layout-main-section .form-inner-toolbar button:nth-child(n+1){float: left !important;}</style>');
+        
+        
 	}
 });
 
