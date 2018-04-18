fcdf373641bf12647104202d449cda38a273b90a
Ammar Harara <ah1995@students.iugaza.edu.ps>
2018-04-19 01:23:35 +0300

    Refactor dev and pmts in project quotation

--- pmo/project_services/doctype/project_quotation/project_quotation.js
+++ pmo/project_services/doctype/project_quotation/project_quotation.js
@@ -1,5 +1,14 @@
 // Copyright (c) 2018, s and contributors
 // For license information, please see license.txt
+
+function getTotalOfField(myfield, mytotalfield, mychildtable, frm) {
+	var total_price = 0;
+	$.each(mychildtable || [], function (i, d) {
+		total_price += flt(d[myfield]) || 0;
+	});
+	frm.set_value(mytotalfield, total_price.toFixed(2));
+}
+
 function getTotal(child) {
 	child.total_cost_price = ((child.cost_price * (1 + (child.overhead_value / 100)))) * (child.months) * (child.quantity / 100);
 }
@@ -23,13 +32,17 @@ function getOverheadExpenses(child) {
 	child.overhead_expenses = (child.quantity / 100) * child.months;
 }
 
-function calculateTechnicalServices(frm, cdn, cdt) {
-	var child = locals[cdn][cdt];
+function calculateTechnicalServices(frm, cdt, cdn) {
+	var child = locals[cdt][cdn];
 	getOverheadExpenses(child);
 	getTotal(child);
 	getSellingPrice(child);
 	getProfit(child);
 	getMargin(child);
+	getTotalOfField('total_cost_price', "total_cost_price_pmts", frm.doc.project_management_and_technical_services, frm);
+	getTotalOfField('selling_price', "total_selling_price_pmts", frm.doc.project_management_and_technical_services, frm);
+	getTotalOfField('profit', "total_profit_pmts", frm.doc.project_management_and_technical_services, frm);
+
 	frm.refresh_fields();
 
 }
@@ -37,24 +50,23 @@ frappe.ui.form.on('Project Management and Technical Services', {
 	refresh: function (frm) {
 
 	},
-	cost_price: function (frm, cdn, cdt) {
-		calculateTechnicalServices(frm, cdn, cdt);
+	cost_price: function (frm, cdt, cdn) {
+		calculateTechnicalServices(frm, cdt, cdn);
+	},
+	months: function (frm, cdt, cdn) {
+		calculateTechnicalServices(frm, cdt, cdn);
 
 	},
-	months: function (frm, cdn, cdt) {
-		calculateTechnicalServices(frm, cdn, cdt);
+	quantity: function (frm, cdt, cdn) {
+		calculateTechnicalServices(frm, cdt, cdn);
 
 	},
-	quantity: function (frm, cdn, cdt) {
-		calculateTechnicalServices(frm, cdn, cdt);
+	total_cost_price: function (frm, cdt, cdn) {
+		calculateTechnicalServices(frm, cdt, cdn);
 
 	},
-	total_cost_price: function (frm, cdn, cdt) {
-		calculateTechnicalServices(frm, cdn, cdt);
-
-	},
-	markup: function (frm, cdn, cdt) {
-		calculateTechnicalServices(frm, cdn, cdt);
+	markup: function (frm, cdt, cdn) {
+		calculateTechnicalServices(frm, cdt, cdn);
 
 	},
 	employee: function (frm, cdt, cdn) {
@@ -77,6 +89,9 @@ frappe.ui.form.on('Project Management and Technical Services', {
 	},
 	designation: function (frm, cdt, cdn) {
 		frappe.model.set_value(cdt, cdn, "employee", "");
+	},
+	total_cost_price: function (frm, cdt, cdn) {
+
 	}
 
 
@@ -89,34 +104,64 @@ frappe.ui.form.on('Project Quotation', {
 	refresh: function (frm) {
 
 	},
-	total_profit:function(frm){
-		if (cur_frm.doc.total_profit && cur_frm.doc.total_cost_price){
+	total_profit: function (frm) {
+		if (cur_frm.doc.total_profit && cur_frm.doc.total_cost_price) {
 			var totals = 0;
-			totals = flt(cur_frm.doc.total_profit) / flt(cur_frm.doc.total_cost_price)*100
-			frm.set_value("total_markup",totals)
-	    }
-	    if (cur_frm.doc.total_profit && cur_frm.doc.total_selling_price){
+			totals = flt(cur_frm.doc.total_profit) / flt(cur_frm.doc.total_cost_price) * 100
+			frm.set_value("total_markup", totals.toFixed(2))
+		}
+		if (cur_frm.doc.total_profit && cur_frm.doc.total_selling_price) {
 			var totals_margin = 0;
-			totals_margin = flt(cur_frm.doc.total_profit) / flt(cur_frm.doc.total_selling_price)*100
-			frm.set_value("total_margin",totals_margin)
-	    }
-        
+			totals_margin = flt(cur_frm.doc.total_profit) / flt(cur_frm.doc.total_selling_price) * 100
+			frm.set_value("total_margin", totals_margin.toFixed(2))
+		}
+
 	},
-	total_cost_price:function(frm){
-		if (cur_frm.doc.total_profit && cur_frm.doc.total_cost_price){
+	total_cost_price: function (frm) {
+		if (cur_frm.doc.total_profit && cur_frm.doc.total_cost_price) {
 			var totals = 0;
-			totals = flt(cur_frm.doc.total_profit) / flt(cur_frm.doc.total_cost_price)*100
+			totals = flt(cur_frm.doc.total_profit) / flt(cur_frm.doc.total_cost_price) * 100
 			console.log(totals);
-			frm.set_value("total_markup",totals)
-	    }
-        
+			frm.set_value("total_markup", totals.toFixed(2))
+		}
+
 	},
-	total_selling_price:function(frm){
-		if (cur_frm.doc.total_profit && cur_frm.doc.total_selling_price){
+	total_selling_price: function (frm) {
+		if (cur_frm.doc.total_profit && cur_frm.doc.total_selling_price) {
 			var totals_margin = 0;
-			totals_margin = flt(cur_frm.doc.total_profit) / flt(cur_frm.doc.total_selling_price)*100
-			frm.set_value("total_margin",totals_margin)
-	    }
+			totals_margin = flt(cur_frm.doc.total_profit) / flt(cur_frm.doc.total_selling_price) * 100
+			frm.set_value("total_margin", totals_margin.toFixed(2))
+		}
+
+	},
+	total_profit_pmts: function (frm) {
+		if (cur_frm.doc.total_profit_pmts && cur_frm.doc.total_cost_price_pmts) {
+			var totals = 0;
+			totals = flt(cur_frm.doc.total_profit_pmts) / flt(cur_frm.doc.total_cost_price_pmts) * 100
+			frm.set_value("total_markup_pmts", totals.toFixed(2))
+		}
+		if (cur_frm.doc.total_profit_pmts && cur_frm.doc.total_selling_price_pmts) {
+			var totals_margin = 0;
+			totals_margin_pmts = flt(cur_frm.doc.total_profit_pmts) / flt(cur_frm.doc.total_selling_price_pmts) * 100
+			frm.set_value("total_margin_pmts", totals_margin_pmts.toFixed(2))
+		}
+
+	},
+	total_cost_price_pmts: function (frm) {
+		if (cur_frm.doc.total_profit_pmts && cur_frm.doc.total_cost_price_pmts) {
+			var totals = 0;
+			totals = flt(cur_frm.doc.total_profit_pmts) / flt(cur_frm.doc.total_cost_price_pmts) * 100
+			console.log(totals);
+			frm.set_value("total_markup_pmts", totals.toFixed(2))
+		}
+
+	},
+	total_selling_price_pmts: function (frm) {
+		if (cur_frm.doc.total_profit_pmts && cur_frm.doc.total_selling_price_pmts) {
+			var totals_margin = 0;
+			totals_margin = flt(cur_frm.doc.total_profit_pmts) / flt(cur_frm.doc.total_selling_price_pmts) * 100
+			frm.set_value("total_margin_pmts", totals_margin.toFixed(2))
+		}
 
 	}
 
@@ -126,123 +171,118 @@ frappe.ui.form.on('Project Quotation', {
 
 
 
-cur_frm.set_query("items_develop", "development_services", function(doc, cdt, cdn) {
-    var d = locals[cdt][cdn];
-    return{
-        filters: [
-            ['Item', 'item_group', '=', d.group_code_develop]
-        ]
-    }
+cur_frm.set_query("items_develop", "development_services", function (doc, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	return {
+		filters: [
+			['Item', 'item_group', '=', d.group_code_develop]
+		]
+	}
 });
 
 
 
 frappe.ui.form.on('Development Services', {
-	currency_develop : function (frm, cdt, cdn){
+	currency_develop: function (frm, cdt, cdn) {
 		var row = locals[cdt][cdn];
-		if ( row.currency_develop == 'SAR' && row.cost_price_develop){
-			frappe.model.set_value(cdt, cdn, "sar_cost_price_develop",row.cost_price_develop);
+		if (row.currency_develop == 'SAR' && row.cost_price_develop) {
+			frappe.model.set_value(cdt, cdn, "sar_cost_price_develop", row.cost_price_develop);
+		} else {
+			frappe.model.set_value(cdt, cdn, "sar_cost_price_develop", row.cost_price_develop * 3.75);
 		}
-        else{
-        	frappe.model.set_value(cdt, cdn, "sar_cost_price_develop",row.cost_price_develop *3.75);
-        }
- 
-	},
-	cost_price_develop: function (frm, cdt, cdn){
-		var row = locals[cdt][cdn];
-		if ( row.currency_develop == 'SAR' && row.cost_price_develop){
-			frappe.model.set_value(cdt, cdn, "sar_cost_price_develop",row.cost_price_develop);
-		}
-        else{
-        	frappe.model.set_value(cdt, cdn, "sar_cost_price_develop",row.cost_price_develop *3.75);
-        }
-        var total_price = 0;
-        $.each(frm.doc.development_services || [], function(i, d) {
-        	console.log('awef')
-            total_price += flt(d.cost_price_develop);
-        });
-        frm.set_value("total_cost_price",total_price);
 
- 
+	},
+	cost_price_develop: function (frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if (row.currency_develop == 'SAR' && row.cost_price_develop) {
+			frappe.model.set_value(cdt, cdn, "sar_cost_price_develop", row.cost_price_develop);
+		} else {
+			frappe.model.set_value(cdt, cdn, "sar_cost_price_develop", row.cost_price_develop * 3.75);
+		}
+		if (row.total_cost_price_develop && row.markup_develop) {
+			frappe.model.set_value(cdt, cdn, "selling_price_develop", row.total_cost_price_develop + (row.total_cost_price_develop * (row.markup_develop / 100)));
+
+		}
+		if (row.selling_price_develop && row.total_cost_price_develop) {
+			frappe.model.set_value(cdt, cdn, "profit_develop", row.selling_price_develop - row.total_cost_price_develop);
+
+		}
+		getTotalOfField("total_cost_price_develop", "total_cost_price", frm.doc.development_services, frm);
+
 	},
 	total_cost_price_develop: function (frm, cdt, cdn) {
-        var row = locals[cdt][cdn];
-        if(row.total_cost_price_develop && row.markup_develop){
-			frappe.model.set_value(cdt, cdn, "selling_price_develop",row.total_cost_price_develop *(row.total_cost_price_develop * (row.markup_develop /100)));
+		var row = locals[cdt][cdn];
+		if (row.total_cost_price_develop && row.markup_develop) {
+			frappe.model.set_value(cdt, cdn, "selling_price_develop", row.total_cost_price_develop + (row.total_cost_price_develop * (row.markup_develop / 100)));
 
-		}   
-		if(row.selling_price_develop && row.total_cost_price_develop){
-			frappe.model.set_value(cdt, cdn, "profit_develop",row.selling_price_develop - row.total_cost_price_develop);
+		}
+		if (row.selling_price_develop && row.total_cost_price_develop) {
+			frappe.model.set_value(cdt, cdn, "profit_develop", row.selling_price_develop - row.total_cost_price_develop);
 
 		}
 
 
-    },
-    markup_develop: function (frm, cdt, cdn) {
-        var row = locals[cdt][cdn];
-        if(row.total_cost_price_develop && row.markup_develop){
-			frappe.model.set_value(cdt, cdn, "selling_price_develop",row.total_cost_price_develop *(row.total_cost_price_develop * (row.markup_develop /100)));
+	},
+	markup_develop: function (frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if (row.total_cost_price_develop && row.markup_develop) {
+			frappe.model.set_value(cdt, cdn, "selling_price_develop", row.total_cost_price_develop + (row.total_cost_price_develop * (row.markup_develop / 100)));
 
 		}
-		if(row.profit_develop && row.markup_develop){
-			frappe.model.set_value(cdt, cdn, "margin_develop",(row.profit_develop / row.selling_price_develop) *100);
+		if (row.profit_develop && row.markup_develop) {
+			frappe.model.set_value(cdt, cdn, "margin_develop", (row.profit_develop / row.selling_price_develop) * 100);
 
 		}
-
-    },
-    quantity_develop: function (frm, cdt, cdn) {
-        var row = locals[cdt][cdn];
-        if(row.quantity_develop && row.sar_cost_price_develop){
-			frappe.model.set_value(cdt, cdn, "total_cost_price_develop",row.quantity_develop * row.sar_cost_price_develop);
+		if (row.selling_price_develop && row.total_cost_price_develop) {
+			frappe.model.set_value(cdt, cdn, "profit_develop", row.selling_price_develop - row.total_cost_price_develop);
 
 		}
 
-    },
-    sar_cost_price_develop: function (frm, cdt, cdn) {
-        var row = locals[cdt][cdn];
-        if(row.quantity_develop && row.sar_cost_price_develop){
-			frappe.model.set_value(cdt, cdn, "total_cost_price_develop",row.quantity_develop * row.sar_cost_price_develop);
+	},
+	quantity_develop: function (frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if (row.quantity_develop && row.sar_cost_price_develop) {
+			frappe.model.set_value(cdt, cdn, "total_cost_price_develop", (row.quantity_develop/100) * row.sar_cost_price_develop);
+
+		}
+		getTotalOfField("total_cost_price_develop", "total_cost_price", frm.doc.development_services, frm);
+
+	},
+	sar_cost_price_develop: function (frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if (row.quantity_develop && row.sar_cost_price_develop) {
+			frappe.model.set_value(cdt, cdn, "total_cost_price_develop", (row.quantity_develop/100) * row.sar_cost_price_develop);
 
 		}
 
-    },
-  
-    profit_develop: function (frm, cdt, cdn) {
-        var row = locals[cdt][cdn];
-        if(row.profit_develop && row.markup_develop){
-			frappe.model.set_value(cdt, cdn, "margin_develop",100 * (row.profit_develop / row.selling_price_develop));
+	},
+
+	profit_develop: function (frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if (row.profit_develop && row.markup_develop) {
+			frappe.model.set_value(cdt, cdn, "margin_develop", 100 * (row.profit_develop / row.selling_price_develop));
 
 		}
-		var total_profit = 0;
-        $.each(frm.doc.development_services || [], function(i, d) {
-        	console.log('awef')
-            total_profit += flt(d.profit_develop);
-        });
-        frm.set_value("total_profit",total_profit);
+		getTotalOfField("profit_develop", "total_profit", frm.doc.development_services, frm);
 
-    },
-    selling_price_develop: function (frm, cdt, cdn) {
-        var row = locals[cdt][cdn];
-        if(row.selling_price_develop && row.total_cost_price_develop){
-			frappe.model.set_value(cdt, cdn, "profit_develop",row.selling_price_develop - row.total_cost_price_develop);
+	},
+	selling_price_develop: function (frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if (row.selling_price_develop && row.total_cost_price_develop) {
+			frappe.model.set_value(cdt, cdn, "profit_develop", row.selling_price_develop - row.total_cost_price_develop);
 
 		}
 
-    },
-    selling_price_develop: function (frm, cdt, cdn) {
-    	var total_selling = 0;
-        $.each(frm.doc.development_services || [], function(i, d) {
-        	console.log('awef')
-            total_selling += flt(d.selling_price_develop);
-        });
-        frm.set_value("total_selling_price",total_selling);
-
-    }
+	},
+	selling_price_develop: function (frm, cdt, cdn) {
+		getTotalOfField("selling_price_develop", "total_selling_price", frm.doc.development_services, frm);
+		
+	}
 
 
- 
 
-    
+
+
 
 });
 
@@ -255,4 +295,3 @@ cur_frm.set_query("employee", 'project_management_and_technical_services', funct
 		}
 	}
 });
-
