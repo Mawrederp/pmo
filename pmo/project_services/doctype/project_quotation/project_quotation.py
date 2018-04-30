b58571f98a179e4c67b662a62d11fbd96c711fd4
Ammar Harara <ah1995@students.iugaza.edu.ps>
2018-04-30 11:50:04 +0300

    ReCode General Pricing View

--- pmo/project_services/doctype/project_quotation/project_quotation.py
+++ pmo/project_services/doctype/project_quotation/project_quotation.py
@@ -12,78 +12,64 @@ class ProjectQuotation(Document):
         ProjectQuotation.create_general_pricing(self)
 
     def create_general_pricing(self):
-
         doc = ""
         exists = False
         items = ["Project Management and Technical Services", "Development Services",
-                 "H/W", "S/W", "Man Power", "Support License Renew", "Training", "Expenses"]
+                 "H/W", "S/W", "Man Power", "Support License Renew", "Training", "Expenses", "Risk & contingency", "Financing", "Commission Of (Sales & P Delivery)", "VAT"]
+
         type_array = ["_pmts", "_develop", "_hw", "_sw",
-                      "_manpower", "_support", "_training", "_expenses"]
+                      "_manpower", "_support", "_training", "_expenses", "_risk_contingency", "_financing", "_commission", "_vat"]
         field_array = ["total_cost_price", "total_selling_price",
                        "total_profit", "total_markup", "total_margin"]
-
-        items_editable = ["Financing",
-                          "Commission Of (Sales & P Delivery)", "VAT"]
-        backup_editable = []
-
-        totals_risk = [0, 0, 0, 0, 0]
-        totals_sums = [0, 0, 0, 0, 0]
+        final_totals_list = [0] * 3
 
         if frappe.db.exists("General Pricing", {"project_q": self.name}):
             exists = True
             gp = frappe.db.sql(
                 """Select name from `tabGeneral Pricing` where project_q = '{0}'""".format(self.name))
             doc = frappe.get_doc("General Pricing", gp[0][0])
-
-            for i in range(len(items_editable)):
-                row = doc.get("project_quotation", {
-                              "items": items_editable[i]})
-                if row:
-                    backup_editable.append(row[0])
-
         else:
             doc = frappe.new_doc("General Pricing")
 
         doc.project_quotation = []
+        
         for i in range(len(items)):
-            totals_risk[0] += float(getattr(self,
-                                            field_array[0]+type_array[i]))
+            for j in range(3):
+                if (getattr(self, field_array[j]+type_array[i])):
+                    final_totals_list[j] += float(
+                        str(getattr(self, field_array[j]+type_array[i])))
+            final_totals_list[1] = round(final_totals_list[1])
+            x = 0
+            if (getattr(self, field_array[1]+type_array[i])):
+                x = getattr(self, field_array[1]+type_array[i])
             doc.append("project_quotation", {"items": items[i],
                                              field_array[0]: getattr(self, field_array[0]+type_array[i]),
-                                             field_array[1]: getattr(self, field_array[1]+type_array[i]),
+                                             field_array[1]: round(float(str(x))),
                                              field_array[2]: getattr(self, field_array[2]+type_array[i]),
                                              field_array[3]: getattr(self, field_array[3]+type_array[i]),
                                              field_array[4]: getattr(self, field_array[4]+type_array[i]),
                                              })
+        selling_price_risk = 0
+        selling_price_financing = 0
+        if(getattr(self, field_array[1]+type_array[8])):
+            selling_price_risk = float(
+                str(getattr(self, field_array[1]+type_array[8])))
+        if(getattr(self, field_array[1]+type_array[9])):
+            selling_price_financing = float(
+                str(getattr(self, field_array[1]+type_array[9])))
 
-        if backup_editable:
-            print(backup_editable)
-            print("************////////////////****************")
-            doc.append("project_quotation", {"items": "Risk & contingency",
-                                             field_array[0]: totals_risk[0]*0.01,
-                                             field_array[1]: int(round(totals_risk[0]*0.01)),
-                                             field_array[2]: totals_risk[2],
-                                             field_array[3]: totals_risk[3],
-                                             field_array[4]: totals_risk[4],
-                                             })
-            for i in range(len(items_editable)):
-                doc.append("project_quotation", backup_editable[i])
-        else:
-            doc.append("project_quotation", {"items": "Risk & contingency",
-                                             field_array[0]: totals_risk[0]*0.01,
-                                             field_array[1]: int(round(totals_risk[0]*0.01)),
-                                             field_array[2]: totals_risk[2],
-                                             field_array[3]: totals_risk[3],
-                                             field_array[4]: totals_risk[4],
-                                             })
-            for i in range(len(items_editable)):
-                doc.append("project_quotation", {"items": items_editable[i],
-                                                 field_array[0]: "0",
-                                                 field_array[1]: "0",
-                                                 field_array[2]: "0",
-                                                 field_array[3]: "0",
-                                                 field_array[4]: "0",
-                                                 })
+        doc.total_cost_price = final_totals_list[0]
+        doc.selling_price = final_totals_list[1]
+        doc.profit_amount = final_totals_list[2]
+        doc.total_markup = (final_totals_list[2] / final_totals_list[0])*100
+        doc.total_margin = round(
+            (final_totals_list[2] / final_totals_list[1])*100, 1)
+        profit_amount_risk = final_totals_list[2] + \
+            selling_price_risk + selling_price_financing
+        doc.profit_amount_risk = profit_amount_risk
+        doc.total_markup_risk = (profit_amount_risk / final_totals_list[0])*100
+        doc.total_margin_risk = round(
+            (profit_amount_risk / final_totals_list[1])*100, 1)
 
         doc.flags.ignore_permissions = True
 
@@ -96,21 +82,12 @@ class ProjectQuotation(Document):
             doc.insert(ignore_permissions=True)
             frappe.msgprint(
                 "<a href='desk#Form/General Pricing/{0}' >{0}</a>".format(doc.name) + " is inserted")
+        self.save()
+        print("ooooooooooooooooooooooooooooooooo")
 
 
 @frappe.whitelist()
 def get_basic_salary(employee):
-    # my_sql_str = ''
-    # emp_sal_str = frappe.db.sql(
-
-    #     """Select rounded_total,start_date from `tabSalary Slip` where employee='{0}' order by start_date desc; """.format(employee), as_dict=True)
-    # if emp_sal_str:
-    #     for i in range(len(emp_sal_str)):
-    #         my_sql_str = frappe.db.sql("Select name from `tabSalary Structure` where is_active='Yes' and name = '{0}'".format(
-    #             emp_sal_str[i].parent), as_dict=True)
-    #         if my_sql_str:
-    #             return emp_sal_str[i].rounded_total
-
     emp_sal = frappe.db.sql(
         "Select rounded_total from `tabSalary Slip` where employee='{0}' order by start_date desc limit 1".format(employee))
     if emp_sal:
