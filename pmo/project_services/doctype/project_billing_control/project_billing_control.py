ded8f90af6f121145ff667537babfee7160078ec
Ahmed Madi <eng.ahmed-madi@hotmail.com>
2018-07-30 13:03:28 +0300

    billing invoice

--- pmo/project_services/doctype/project_billing_control/project_billing_control.py
+++ pmo/project_services/doctype/project_billing_control/project_billing_control.py
@@ -8,17 +8,18 @@ from frappe.model.document import Document
 
 class ProjectBillingControl(Document):
 	def make_invoice(self,project_name,scope_item,items_value,billing_percentage):
-		frappe.get_doc({
-			"doctype":"Sales Invoice",
-			"customer": 'Ahmed',
-			"project": project_name,
-			"items": [
-				  {
-					"doctype": "Sales Invoice Item",
-					"item_code": "02-01-160-2101",
-					"rate": items_value,
-					"qty": billing_percentage/100
-				  }
-				]
+		pass
+		# frappe.get_doc({
+		# 	"doctype":"Sales Invoice",
+		# 	"customer": 'C',
+		# 	"project": project_name,
+		# 	"items": [
+		# 		  {
+		# 			"doctype": "Sales Invoice Item",
+		# 			"item_code": "ITEM-00006",
+		# 			"rate": items_value,
+		# 			"qty": billing_percentage/100
+		# 		  }
+		# 		]
 			
-		}).insert(ignore_permissions=True)
+		# }).insert(ignore_permissions=True)
