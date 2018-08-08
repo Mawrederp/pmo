80171addc97118812526df61060e7c561a90b28b
Omar Jaber <omar.ja93@gmail.com>
2018-08-08 10:09:17 +0300

    project costing and procuprocurment control

--- pmo/project_services/doctype/project_costing_control/project_costing_control.js
+++ pmo/project_services/doctype/project_costing_control/project_costing_control.js
@@ -4,5 +4,29 @@
 frappe.ui.form.on('Project Costing Control', {
 	refresh: function(frm) {
 
+	},
+	project_name: function(frm) {
+		if(cur_frm.doc.project_name){
+			cur_frm.doc.project_costing_schedule_control = []
+			frappe.model.with_doc("Project Initiation", frm.doc.project_name, function() {
+		        var tabletransfer= frappe.model.get_doc("Project Initiation", frm.doc.project_name)
+		        frm.doc.project_costing_schedule_control = []
+	            frm.refresh_field("project_costing_schedule_control");
+		        $.each(tabletransfer.project_costing_schedule, function(index, row){
+		        	if(row.type_of_cost=='Tawari Services'){
+			            d = frm.add_child("project_costing_schedule_control");
+			            d.type_of_cost = row.type_of_cost;
+			            d.project_cost_value = row.project_cost_value;
+			            d.description_comments = row.description_comments;
+			            d.delivery_date_period = row.delivery_date_period;
+			            d.delivery_date = row.delivery_date;
+			            d.delivery_period_from_date = row.delivery_period_from_date;
+			            d.delivery_period_to_date = row.delivery_period_to_date;
+			            frm.refresh_field("project_costing_schedule_control");
+			        }
+		        });
+		    })
+		}
 	}
+
 });
--- pmo/project_services/doctype/projects_procurement_control/projects_procurement_control.js
+++ pmo/project_services/doctype/projects_procurement_control/projects_procurement_control.js
@@ -4,5 +4,36 @@
 frappe.ui.form.on('Projects Procurement Control', {
 	refresh: function(frm) {
 
+	},
+	project_name: function(frm) {
+		if(cur_frm.doc.project_name){
+			cur_frm.doc.project_costing_schedule_control = []
+			frappe.model.with_doc("Project Initiation", frm.doc.project_name, function() {
+		        var tabletransfer= frappe.model.get_doc("Project Initiation", frm.doc.project_name)
+		        frm.doc.project_costing_schedule_control = []
+	            frm.refresh_field("project_costing_schedule_control");
+		        $.each(tabletransfer.project_costing_schedule, function(index, row){
+		        	if(row.type_of_cost=='External Expenses'){
+			            d = frm.add_child("project_costing_schedule_control");
+			            d.type_of_cost = row.type_of_cost;
+			            d.description_comments = row.description_comments;
+			            d.scope_item = row.scope_item;
+			            d.project_cost_value = row.project_cost_value;
+			            d.scope_item_cost_value = row.scope_item_cost_value;
+			            d.no_contracts = row.no_contracts;
+			            d.po_contract_extimated_cost = row.po_contract_extimated_cost;
+			            d.vendor = row.vendor;
+			            d.last_date = row.last_date;
+			            d.delivery_date_period = row.delivery_date_period;
+			            d.delivery_date = row.delivery_date;
+			            d.delivery_period_from_date = row.delivery_period_from_date;
+			            d.delivery_period_to_date = row.delivery_period_to_date;
+			            frm.refresh_field("project_costing_schedule_control");
+			        }
+		        });
+		    })
+		}
 	}
+
+
 });
