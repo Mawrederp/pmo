b6012ffaab4525a301befa6134c2d724e49ee940
Omar Jaber <omar.ja93@gmail.com>
2018-08-13 14:38:45 +0300

    remove delivery note from billing control

--- pmo/project_services/doctype/project_billing_control/project_billing_control.js
+++ pmo/project_services/doctype/project_billing_control/project_billing_control.js
@@ -125,10 +125,6 @@ frappe.ui.form.on('Project Billing Control', {
 
 
 
-		                    
-
-
-
 		                }
 			        });
 
@@ -137,117 +133,7 @@ frappe.ui.form.on('Project Billing Control', {
     	});
 
 
-
-
-    	frm.add_custom_button(__("Make Delivery Note"), function () {
-			// items = []
-			for(row= 0;row<cur_frm.doc.project_payment_schedule_control.length;row++){
-				if(cur_frm.doc.project_payment_schedule_control[row].invoice == 1){
-					
-					var scope_item = cur_frm.doc.project_payment_schedule_control[row].scope_item
-					if(scope_item){
-						scope_item=scope_item
-					}else{
-						scope_item=''
-					}
-
-					var project_name = cur_frm.doc.project_name
-					if(project_name){
-						project_name=project_name
-					}else{
-						project_name=''
-					}
-
-					var items_value = cur_frm.doc.project_payment_schedule_control[row].items_value
-					if(items_value){
-						items_value=items_value
-					}else{
-						items_value=0
-					}
-
-					var billing_percentage = cur_frm.doc.project_payment_schedule_control[row].billing_percentage
-					if(billing_percentage){
-						billing_percentage=billing_percentage
-					}else{
-						billing_percentage=0
-					}
-
-
-					var description_when = cur_frm.doc.project_payment_schedule_control[row].description_when
-					if(description_when){
-						description_when=description_when
-					}else{
-						description_when=''
-					}
-
-					var vat_value = cur_frm.doc.project_payment_schedule_control[row].vat_value
-					if(vat_value){
-						vat_value=vat_value
-					}else{
-						vat_value=0
-					}
-
-					var billing_status = cur_frm.doc.project_payment_schedule_control[row].billing_status
-					if(billing_status){
-						billing_status=billing_status
-					}else{
-						billing_status=0
-					}
-
-					frappe.call({
-			            "method": "make_delivery_note",
-			            doc: cur_frm.doc,
-			            args: { "scope_item": scope_item,"project_name": project_name,
-			            		"items_value": items_value,"billing_percentage": billing_percentage,
-			            		"description_when":description_when,"vat_value":vat_value,"billing_state":billing_status},
-			            callback: function (r) {
-			            	delivery_note_name = r.message
-     						$.each(frm.doc.project_payment_schedule_control || [], function(i, v) {
-     							if(v.invoice){
-     								frappe.model.set_value(v.doctype, v.name, "billing_status", 1)
-	     							frappe.model.set_value(v.doctype, v.name, "delivery_note", delivery_note_name)
-	     						
-
-
-	     							frappe.call({
-							            "method": "updat_init_payment_table",
-							            doc: cur_frm.doc,
-							            args: {"itm": v.scope_item,"idx": v.idx,"delivery_note":delivery_note_name},
-							            callback: function (r) {
-							            	if(r.message){
-							            		console.log(r.message)
-		     								}
-
-						                }
-							        });
-
-
-	     							// frappe.call({
-							      //       "method": "get_init_payment_name",
-							      //       doc: cur_frm.doc,
-							      //       args: {"itm": v.scope_item,"idx": v.idx,"delivery_note":delivery_note_name},
-							      //       callback: function (r) {
-							      //       	if(r.message){
-
-								     //        	frappe.model.set_value('Project Payment Schedule', r.message, "billing_status", 1)
-		     						// 			frappe.model.set_value('Project Payment Schedule', r.message, "delivery_note", delivery_note_name)
-		     						// 		}
-
-						       //          }
-							      //   });
-
-
-								}		
-							})
-
-		                }
-			        });
-
-				}
-			}
-
-    	});
-
+    	
 
 	},
 	project_name: function(frm) {
@@ -275,7 +161,6 @@ frappe.ui.form.on('Project Billing Control', {
 		            d.when = row.when;
 		            d.description_when = row.description_when;
 		            d.billing_status = row.billing_status;
-		            d.delivery_note = row.delivery_note;
 		            d.sales_invoice = row.sales_invoice;
 		            frm.refresh_field("project_payment_schedule_control");
 		        });
--- pmo/project_services/doctype/project_billing_control/project_billing_control.py
+++ pmo/project_services/doctype/project_billing_control/project_billing_control.py
@@ -10,29 +10,6 @@ from frappe.utils import cint, cstr, date_diff, flt, formatdate, getdate, get_li
 	comma_or, get_fullname
 
 class ProjectBillingControl(Document):
-	# def validate(self):
-		# self.get_total_so_fat()
-
-
-	# def get_total_so_fat(self):
-	# 	total_scope_item = frappe.db.sql("""select project_name from `tabProject Billing Control` """)
-	# 	for row in self.project_payment_schedule_control:
-	# 		if row.billing_status and row.invoice:
-	# 			row.invoice=0
-	# 			frappe.throw("There is already invoice for this item")
-	# 		elif row.invoice:
-	# 			if total_scope_item :
-	# 				self.total_scope_item_billing_so_far = frappe.db.sql("""select sum(payment.items_value) from `tabProject Payment Schedule` payment
-	# 	 			join `tabProject Billing Control` billing on payment.parent=billing.name where payment.parenttype='Project Billing Control' 
-	# 	 			and billing.project_name='{0}' and payment.invoice=1 and payment.scope_item='{1}' """.format(self.project_name,row.scope_item))[0][0]
-
-	# 				self.total_project_billing_so_far = frappe.db.sql("""select sum(payment.items_value) from `tabProject Payment Schedule` payment
-	# 	 			join `tabProject Billing Control` billing on payment.parent=billing.name where payment.parenttype='Project Billing Control' 
-	# 	 			and billing.project_name='{0}' and payment.invoice=1 """.format(self.project_name))[0][0]
-
-	# 				row.billing_status=1		 			
-
-
 
 	def make_invoice(self,project_name,scope_item,items_value,billing_percentage,due_date,description_when,vat_value,billing_state,sales_invoice):
 		arr=[]
@@ -103,99 +80,6 @@ class ProjectBillingControl(Document):
 
 
 
-
-	def make_delivery_note(self,project_name,scope_item,items_value,billing_percentage,description_when,vat_value,billing_state):
-		arr=[]
-		for row in self.project_payment_schedule_control:
-			if row.invoice==1:
-				arr.append(row.name)
-
-		if arr and len(arr)==1 and billing_state!=1:
-			if not frappe.db.exists("Item", {"item_name": scope_item }):
-				doc = frappe.new_doc("Item")
-				doc.item_group = 'Project'
-				doc.item_code = scope_item
-				doc.item_name = scope_item
-				doc.is_stock_item = 0
-				doc.flags.ignore_validate = True
-				doc.flags.ignore_mandatory = True
-				doc.insert(ignore_permissions=True)
-
-
-			item_name = frappe.get_value("Item", filters = {"item_name": scope_item}, fieldname = "name")    
-
-			customer = frappe.db.sql("select customer from `tabProject Initiation` where name='{0}' ".format(self.project_name))
-
-			if customer:
-				sinv=frappe.get_doc({
-					"doctype":"Delivery Note",
-					"customer": customer[0][0],
-					# "project": project_name,
-					"naming_series": 'DN-',
-					"workflow_state": 'Pending',
-					"project": project_name,
-					# "due_date": due_date,
-					# "debit_to": 'Debtors - O',
-					"items": [
-						  {
-							"doctype": "Delivery Note Item",
-							"item_code": item_name,
-							"description": description_when,
-							"qty": flt(flt(billing_percentage)/100),
-							"rate": items_value
-						  }
-						],
-					"taxes": [
-						  {
-							"doctype": "Sales Taxes and Charges",
-							"charge_type": 'Actual',
-							"description": description_when,
-							"tax_amount": vat_value
-						  }
-						]
-				})
-				# sinv.flags.ignore_validate = True
-				sinv.flags.ignore_mandatory = True
-				sinv.insert(ignore_permissions=True)
-
-				frappe.msgprint("Delivery Note is created")
-			else:
-				frappe.throw('You sould select customer for this project before issue Delivery Note')
-		else:
-			frappe.throw("You should check one invoice to make Delivery Note, or this item may have delivery note")
-
-		# init_payment_name = frappe.db.sql("""
-		# 	select payment.name from `tabProject Payment Schedule` payment join `tabProject Initiation` init on payment.parent=init.name
-		# 	where payment.parenttype='Project Initiation' and init.project_name='{0}' and payment.scope_item='data23'
-		# 	and payment.idx='2'
-		# 	""".format(self.project_name,)) 
-
-		return sinv.name
-		
-
-
-
-	def updat_init_payment_table(self,delivery_note,itm,idx):
-		init_payment_name = ''
-		init_payment_name = frappe.db.sql("""
-	 	select payment.name from `tabProject Payment Schedule` payment join `tabProject Initiation` init on payment.parent=init.name
-	 	where payment.parenttype='Project Initiation' and init.name='{0}' and payment.scope_item='{1}'
-	 	and payment.idx='{2}'
-	 	""".format(self.project_name,itm,idx)) 
-	 	if init_payment_name:
-	 		init_payment_name=init_payment_name[0][0]
-
-	 	doc = frappe.get_doc("Project Payment Schedule",init_payment_name)
-		doc.billing_status = 1
-		doc.delivery_note = delivery_note
-		doc.flags.ignore_mandatory = True
-		doc.save(ignore_permissions=True)
-
- 		return init_payment_name
-
-
-
-
 	def updat_init_payment_table_invoice(self,sales_invoice,itm,idx):
 		init_payment_name = ''
 		init_payment_name = frappe.db.sql("""
@@ -243,28 +127,6 @@ class ProjectBillingControl(Document):
 			total_item = 0
 
 		return total_project,total_item,total_project_count
-
-
-
-	# def get_init_payment_name(self,delivery_note,itm,idx):
-	# 	init_payment_name = ''
-	# 	init_payment_name = frappe.db.sql("""
-	#  	select payment.name from `tabProject Payment Schedule` payment join `tabProject Initiation` init on payment.parent=init.name
-	#  	where payment.parenttype='Project Initiation' and init.name='{0}' and payment.scope_item='{1}'
-	#  	and payment.idx='{2}'
-	#  	""".format(self.project_name,itm,idx)) 
-	#  	if init_payment_name:
-	#  		init_payment_name=init_payment_name[0][0]
-
-	#  	update_payment =frappe.db.sql("""
-	#  	update `tabProject Payment Schedule` payment join `tabProject Initiation` init on payment.parent=init.name
-	#  	set payment.billing_status=1,payment.delivery_note='{0}' where payment.parenttype='Project Initiation' and init.name='{1}' and payment.scope_item='{2}'
-	#  	and payment.idx='{3}'
-	#  	""".format(delivery_note,self.project_name,itm,idx))
-
- # 		return init_payment_name
-
-
 
 
 
